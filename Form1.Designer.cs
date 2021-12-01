dbe0d24aeb7b2fa5555d39c85733817528b0e106
Christopher Burgdorff <cburgdorff@pringle-quinn.com>
2021-12-01 17:23:35 -0500

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Bates_Verifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.lstMain = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.chkFileNameOnly = new System.Windows.Forms.CheckBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.fbd1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblBatesPrefix = new System.Windows.Forms.Label();
+            this.txtBatesPrefix = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(176, 42);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(476, 20);
+            this.txtFolder.TabIndex = 0;
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(700, 24);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(114, 50);
+            this.btnVerify.TabIndex = 1;
+            this.btnVerify.Text = "Get Bates Numbers";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // lstMain
+            // 
+            this.lstMain.HideSelection = false;
+            this.lstMain.Location = new System.Drawing.Point(12, 94);
+            this.lstMain.Name = "lstMain";
+            this.lstMain.Size = new System.Drawing.Size(1279, 445);
+            this.lstMain.TabIndex = 2;
+            this.lstMain.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folder:";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(1134, 545);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(157, 50);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "E&xport to Excel";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // chkFileNameOnly
+            // 
+            this.chkFileNameOnly.AutoSize = true;
+            this.chkFileNameOnly.Location = new System.Drawing.Point(910, 42);
+            this.chkFileNameOnly.Name = "chkFileNameOnly";
+            this.chkFileNameOnly.Size = new System.Drawing.Size(97, 17);
+            this.chkFileNameOnly.TabIndex = 5;
+            this.chkFileNameOnly.Text = "File Name Only";
+            this.chkFileNameOnly.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(8, 24);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(117, 50);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblBatesPrefix
+            // 
+            this.lblBatesPrefix.AutoSize = true;
+            this.lblBatesPrefix.Location = new System.Drawing.Point(1101, 46);
+            this.lblBatesPrefix.Name = "lblBatesPrefix";
+            this.lblBatesPrefix.Size = new System.Drawing.Size(63, 13);
+            this.lblBatesPrefix.TabIndex = 7;
+            this.lblBatesPrefix.Text = "Bates Prefix";
+            // 
+            // txtBatesPrefix
+            // 
+            this.txtBatesPrefix.Location = new System.Drawing.Point(1170, 43);
+            this.txtBatesPrefix.Name = "txtBatesPrefix";
+            this.txtBatesPrefix.Size = new System.Drawing.Size(123, 20);
+            this.txtBatesPrefix.TabIndex = 8;
+            this.txtBatesPrefix.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 607);
+            this.Controls.Add(this.txtBatesPrefix);
+            this.Controls.Add(this.lblBatesPrefix);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.chkFileNameOnly);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstMain);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.txtFolder);
+            this.Name = "Form1";
+            this.Text = "Bates Verifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.ListView lstMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.CheckBox chkFileNameOnly;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog fbd1;
+        private System.Windows.Forms.Label lblBatesPrefix;
+        private System.Windows.Forms.TextBox txtBatesPrefix;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using iTextSharp;
+using iTextSharp.text.pdf;
+
+namespace Bates_Verifier
+{
+    public partial class Form1 : Form
+    {
+        private void InitializeMainListView()
+        {        
+            InitializeComponent();
+            lstMain.Columns.Add("File Name");
+            lstMain.Columns.Add("Starting Bates");
+            lstMain.Columns.Add("Ending Bates");
+            lstMain.View = View.Details;
+            lstMain.Columns[0].Width = 975;
+            lstMain.Columns[1].Width = 150;
+            lstMain.Columns[2].Width = 150;
+        }
+        
+        public Form1()
+        {
+            InitializeMainListView();
+        }
+
+        public void EnableAllElements()
+        {
+            btnBrowse.Enabled = true;
+            btnExport.Enabled = true;
+            btnVerify.Enabled = true;
+            txtBatesPrefix.Enabled = true;
+            txtFolder.Enabled = true;
+        }
+
+        public void DisableAllElements()
+        {
+            btnBrowse.Enabled = false;
+            btnExport.Enabled = false;
+            btnVerify.Enabled = false;
+            txtBatesPrefix.Enabled = false;
+            txtFolder.Enabled = false;
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(txtFolder.Text);
+
+            DisableAllElements();
+            string folderPath = txtFolder.Text;
+            string fileName = "";
+            string startingBates = "";
+            string fileNameOnly = "";
+            string batesPrefix = txtBatesPrefix.Text;
+            string endingBates = "";
+
+            if (folderPath[folderPath.Length - 1] != '\\' )
+            {
+                folderPath = folderPath + @"\";
+            }
+
+            if (!Directory.Exists(folderPath))
+            {
+                MessageBox.Show("Invalid Folder.");
+                EnableAllElements();
+                return;
+            }
+            if (txtBatesPrefix.Text.Length < 3)
+            {
+                MessageBox.Show("Invalid Bates Prefix");
+                EnableAllElements();
+                return;
+            }
+            lstMain.Items.Clear();
+
+            string[] allPDFs = Directory.GetFiles(folderPath, "*.pdf", SearchOption.TopDirectoryOnly);
+
+            for (int i = 0; i < allPDFs.Length; i++)
+            {
+                fileName = allPDFs[i];
+                startingBates = "";
+
+                var reader = new PdfReader(File.ReadAllBytes(fileName));
+                var contentBytes = reader.GetPageContent(1);
+                var tokenizer = new PRTokeniser(new RandomAccessFileOrArray(contentBytes));
+                var stringsList = new List<string>();
+                try
+                {
+                    while (tokenizer.NextToken())
+                    {
+                        if (tokenizer.TokenType == PRTokeniser.TokType.STRING)
+                        {
+                            //stringsList.Add(tokenizer.StringValue);
+                            if (tokenizer.StringValue.Contains(batesPrefix))
+                            {
+                                startingBates = tokenizer.StringValue.Trim();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    startingBates = ex.Message;
+                }
+                
+
+
+                //GET LAST BATES NUMBER
+                var contentBytesLast = reader.GetPageContent(reader.NumberOfPages);
+                var tokenizerLast = new PRTokeniser(new RandomAccessFileOrArray(contentBytesLast));
+                var stringsListLast = new List<string>();
+                try
+                {
+                    while (tokenizerLast.NextToken())
+                    {
+                        if (tokenizerLast.TokenType == PRTokeniser.TokType.STRING)
+                        {
+                            //stringsList.Add(tokenizer.StringValue);
+                            if (tokenizerLast.StringValue.Contains(batesPrefix))
+                            {
+                                endingBates = tokenizerLast.StringValue.Trim();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    endingBates = ex.Message;
+                }
+
+                if (chkFileNameOnly.Checked)
+                {
+                    fileNameOnly = Path.GetFileName(fileName);
+                    lstMain.Items.Add(new ListViewItem(new string[] { fileNameOnly, startingBates, endingBates }));
+                }
+                else
+                {
+                    lstMain.Items.Add(new ListViewItem(new string[] { fileName, startingBates, endingBates }));
+                }
+            }
+            EnableAllElements();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            DisableAllElements();
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx", ValidateNames = true })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+                    Workbook wb = app.Workbooks.Add(XlSheetType.xlWorksheet);
+                    Worksheet ws = (Worksheet)app.ActiveSheet;
+                    app.Visible = false;
+                    ws.Cells[1, 1] = "File Name";
+                    ws.Cells[1, 2] = "Starting Bates";
+                    ws.Cells[1, 3] = "Ending Bates";
+                    int i = 2;
+                    foreach (ListViewItem item in lstMain.Items)
+                    {
+                        ws.Cells[i, 1] = item.SubItems[0].Text;
+                        ws.Cells[i, 2] = item.SubItems[1].Text;
+                        ws.Cells[i, 3] = item.SubItems[2].Text;
+                        i++;
+                    }
+                    wb.SaveAs(sfd.FileName, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+                    app.Quit();
+                    MessageBox.Show("Export Successful!");
+                }                
+            }
+            EnableAllElements();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DisableAllElements();
+            if (fbd1.ShowDialog() == DialogResult.OK)
+            {
+                txtFolder.Text = fbd1.SelectedPath;
+            }
+            EnableAllElements();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
